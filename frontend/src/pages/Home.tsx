5b3a78fc0c7b8cd65b86eebf47331459250cf6c3
startcroft <santiagogomezpavas42@gmail.com>
2026-02-05 13:38:41 -0500

    feat: implement Home page layout and add button functionality

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,35 +1,60 @@
+import { useRef } from 'react';
 import VolumeList from "../shared/components/VolumeList"
 import type { Volume } from "../shared/types/volume";
+import './Home.scss'
 
 export const Home = () => {
-      const volumeList: Volume[] = [
-        {
-          id: '1',
-          title: 'Volume One',
-          description: 'Description One',
-          categories: ['Historia', 'Ciencia'],
-          price: 10,
-          thumbnail: 'image1.jpg',
-        },
-        {
-          id: '2',
-          title: 'Volume Two',
-          description: 'Description Two',
-          categories: ['Filosofía'],
-          price: 20,
-          thumbnail: 'image2.jpg',
-        },
-        {
-          id: '3',
-          title: 'Volume Three',
-          description: 'Description Three',
-          categories: ['Arte', 'Tecnología'],
-          price: 30,
-          thumbnail: 'image3.jpg',
-        },
-      ];
+  const videoListRef = useRef<HTMLDivElement>(null);
 
-    return(
+  const handleScroll = () => {
+    if (videoListRef.current) {
+      videoListRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const volumeList: Volume[] = [
+    {
+      id: '1',
+      title: 'Volume One',
+      description: 'Description One',
+      categories: ['Historia', 'Ciencia'],
+      price: 10,
+      thumbnail: 'image1.jpg',
+    },
+    {
+      id: '2',
+      title: 'Volume Two',
+      description: 'Description Two',
+      categories: ['Filosofía'],
+      price: 20,
+      thumbnail: 'image2.jpg',
+    },
+    {
+      id: '3',
+      title: 'Volume Three',
+      description: 'Description Three',
+      categories: ['Arte', 'Tecnología'],
+      price: 30,
+      thumbnail: 'image3.jpg',
+    },
+  ];
+
+  return (
+    <div className='home-container'>
+      <div className='home-banner' >
+
+        <button
+        className='banner-button'
+          onClick={handleScroll}
+        >
+          Empieza a explorar
+        </button>
+      </div>
+
+      
+      <div className="video-list-section" data-testid="video-list-section" ref={videoListRef}>
         <VolumeList volumes={volumeList}/>
-    )
+      </div>
+    </div>
+  );
 }
