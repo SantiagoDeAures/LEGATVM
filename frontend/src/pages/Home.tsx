5649a869b642687cc001c5b93a1b5e3e7364d4ae
startcroft <santiagogomezpavas42@gmail.com>
2026-02-23 16:25:22 -0500

    fix banner

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -58,7 +58,7 @@ useEffect(() => {
       <div className='home-banner' >
 
         <video autoPlay muted loop className="background-video">
-          <source src='../src/assets/banner-LEGATVM.mp4' type='video/mp4' />
+          <source src='https://res.cloudinary.com/dzbllqpfj/video/upload/v1771881705/banner-LEGATVM_rzdurg.mp4' type='video/mp4' />
         </video>
 
         <button
